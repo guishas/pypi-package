50a443396df52840a8da5ac8d3688a8f6fb0eae3
guishas <lunettagui@gmail.com>
2022-09-06 14:22:57 -0300

    uploaded to test pypi

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ from setuptools import setup
 setup(
   name="dev_aberto_gui",
   version=0.1,
-  packages=['dev_aberto']
+  packages=['dev_aberto'],
+  install_requires=[
+    'requests'
+  ],
+  scripts=['scripts/hello.py']
 )
